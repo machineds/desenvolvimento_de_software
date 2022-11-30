eaa6b7c557af339d8c1386fa9ff8edc718a9bc67
dsmachine <ds102turma@gmail.com>
2022-11-30 11:30:40 -0300

    Implementação de um script que recebe 2 inteiros, multiplica e ainda informa o maior valor permitido, com base na multiplicação, função Number.MAX_VALUE

---
+++ Exercices/javascript/exercice6.js
@@ -0,0 +1,4 @@
+var usuario = prompt('Digite o seu nome:')
+var nome_aluno = "Zêder"
+alert('Oi,' + usuario + 'meu nome é' + nome_aluno)
+alert(`Oi, ${usuario} meu nome é ${nome_aluno}.`)
---
+++ Exercices/javascript/exercice7.js
@@ -0,0 +1,4 @@
+var pergunta1 = prompt("Insira o primeiro número inteiro:")
+var pergunta2 = prompt("Insira o segundo número inteiro:")
+var multiply = pergunta1 * pergunta2
+alert(`A multiplicação entre ${pergunta1} e ${pergunta2} é: ${multiply}`)
