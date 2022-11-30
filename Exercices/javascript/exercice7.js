338290933afc65171ec108d22a2bf7f97640cdb4
Laboratório Caxias - Alunos <labcaxias@LABDC.ZENTYAL>
2022-11-30 14:33:54 -0300

    Site para praticar funções do javascript

--- Exercices/javascript/exercice7.js
+++ Exercices/javascript/exercice7.js
@@ -2,3 +2,4 @@ var pergunta1 = prompt("Insira o primeiro número inteiro:")
 var pergunta2 = prompt("Insira o segundo número inteiro:")
 var multiply = pergunta1 * pergunta2
 alert(`A multiplicação entre ${pergunta1} e ${pergunta2} é: ${multiply}`)
+alert(Number.MAX_VALUE)
---
+++ anotacoes_aulas/minprojeto1/javascript/aula_30_11.js
@@ -0,0 +1,4 @@
+var n1 = number(prompt('Insira o primeiro número:'))
+var n2 = number(prompt('Insira o segundo número:'))
+var result = n1 * n2
+document.write(`O resultado da multiply entre ${n1} e ${n2} = ${result.toFixed(2)}`)
