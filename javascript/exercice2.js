822a5b5586ba3ea165dbf381696eb725e9e01407
dsmachine <ds102turma@gmail.com>
2022-11-30 10:53:49 -0300

    Caclcúlo com operações at

---
+++ javascript/exercice2.js
@@ -0,0 +1,7 @@
+var n1=2; n2=5; 
+var soma = n1+n2
+var diferenca = n1-n2
+var multiply = n1*n2
+var div = n1/n2
+var mod = n1%n2
+alert(`Soma: ${n1} + ${n2} = ${soma}  \nDiferença: ${n1} - ${n2} = ${diferenca} \nMultiply: ${n1} * ${n2} = ${multiply} \nDivisão: ${n1} / ${n2} = ${div} \nMódulo: ${n1} % ${n2} = ${mod}`)
